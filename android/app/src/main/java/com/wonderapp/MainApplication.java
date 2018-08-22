d438f4c84075787989dc2abe90a7fa03ac87eb29
DmitrySokolovsky <dmitry_s_i@mail.ru>
2018-08-22 09:39:39 +0300

    add .kt

--- android/app/src/main/java/com/wonderapp/MainApplication.java
+++ android/app/src/main/java/com/wonderapp/MainApplication.java
@@ -5,6 +5,7 @@ import android.app.Application;
 import com.facebook.react.ReactApplication;
 import io.invertase.firebase.RNFirebasePackage;
 import io.invertase.firebase.firestore.RNFirebaseFirestorePackage;
+import io.invertase.firebase.auth.RNFirebaseAuthPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -29,6 +30,7 @@ public class MainApplication extends Application implements ReactApplication {
           new MainReactPackage(),
           new RNFirebasePackage(),
           new RNFirebaseFirestorePackage(),
+          new RNFirebaseAuthPackage(),
           new VectorIconsPackage()
       );
     }
